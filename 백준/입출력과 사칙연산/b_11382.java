758448c7eca967c7ea470c8d22a431039850ce31
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-07-06 17:41:08 +0900

    Add files via upload

---
+++ 백준/입출력과 사칙연산/b_11382.java
@@ -0,0 +1,18 @@
+package b_problem;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/11382
+
+public class b_11382 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A, B, C;
+		A = sc.nextLong();
+		B = sc.nextLong();
+		C = sc.nextLong();
+		System.out.println(A + B + C);
+	}
+
+}
