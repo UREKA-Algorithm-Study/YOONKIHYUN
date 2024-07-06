b24bcdf78dee48f62276c392ff176a245bb2dc7b
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-07-06 17:56:17 +0900

    Add files via upload

---
+++ 백준/입출력과 사칙연산/b_10171.java
@@ -0,0 +1,13 @@
+package b_problem;
+
+import java.util.Scanner;
+
+public class b_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+}
