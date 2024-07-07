79c4d4d1b9c2f5ffda21c8835ba074961cb82bf4
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-07-07 23:16:29 +0900

    Add files via upload

---
+++ 백준/조건문/b_1330.java
@@ -0,0 +1,20 @@
+package b_problem;
+
+import java.util.Scanner;
+
+public class b_1330 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A > B)
+			System.out.println(">");
+		else if (A < B)
+			System.out.println("<");
+		else
+			System.out.println("==");
+	}
+}
