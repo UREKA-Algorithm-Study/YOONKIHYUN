56346527b2f4bb4003ecdf18bfa8b5e17530a989
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-07-30 23:33:33 +0900

    Add files via upload

---
+++ 백준/조건문/b_14681.java
@@ -0,0 +1,27 @@
+package b_problem;
+
+import java.util.Scanner;
+
+public class b_14681 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if (x > 0 && y > 0) {
+			System.out.println(1);
+		}
+		if (x < 0 && y > 0) {
+			System.out.println(2);
+		}
+		if (x < 0 && y < 0) {
+			System.out.println(3);
+		}
+		if (x > 0 && y < 0) {
+			System.out.println(4);
+		}
+		
+	}
+}
