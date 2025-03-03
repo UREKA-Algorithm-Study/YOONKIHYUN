e9a114dcd4a5859b3424ed31b7511a781b7e6fa9
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-03 22:21:22 +0900

    Update b_2884.java

--- 백준/조건문/b_2884.java
+++ 백준/조건문/b_2884.java
@@ -1,26 +1,26 @@
-package b_problem;
+package b_2;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class b_2884 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int H = sc.nextInt(); // 시
-		int M = sc.nextInt(); // 분
-
+		
+		int H = sc.nextInt();	//시
+		int M = sc.nextInt();	//분
+		
 		M -= 45;
-
-		if (M < 0) {
+		
+		if(M < 0) {
 			H--;
 			M += 60;
 		}
-
-		if (H < 0) {
+		
+		if(H < 0) {
 			H = 23;
 		}
-
+		
 		System.out.println(H + " " + M);
 	}
 }
