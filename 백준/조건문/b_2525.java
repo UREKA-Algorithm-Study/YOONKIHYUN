785b3214469422cace8fc95e8146dae2c1c62f0f
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-03 22:44:59 +0900

    Update b_2525.java

--- 백준/조건문/b_2525.java
+++ 백준/조건문/b_2525.java
@@ -1,27 +1,27 @@
-package b_problem;
+package b_2;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class b_2525 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int H = sc.nextInt(); // 시
-		int M = sc.nextInt(); // 분
-		int C = sc.nextInt(); // 필요한 시간(분 단위)
-
-		M += C;
-
-		while (M >= 60) {
+		
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		int time = sc.nextInt();
+				
+		M += time;
+		
+		while(M >= 60) {
 			M -= 60;
 			H++;
 		}
-
-		if (H >= 24) {
+		
+		if(H >= 24) {
 			H -= 24;
 		}
-
+		
 		System.out.println(H + " " + M);
 	}
 }
