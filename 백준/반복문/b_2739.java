1eda858e224fdcdc9434a419f5bca6bc1c650594
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-04 23:15:47 +0900

    Update b_2739.java

--- 백준/반복문/b_2739.java
+++ 백준/반복문/b_2739.java
@@ -1,17 +1,16 @@
-package b_problem;
+package b_3;
 
 import java.util.Scanner;
 
 public class b_2739 {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		
-		for (int i = 1; i <= 9; i++) {
-	
+
+		for(int i=1; i<=9; i++) {		
 			System.out.println(N+ " * " +i+ " = " +(N*i));
 		}
-		
 	}
 }
