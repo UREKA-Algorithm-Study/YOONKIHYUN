691ddc44a688ad21cd48b6ce16b51993a27a31e3
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-04 23:41:27 +0900

    Update b_10950.java

--- 백준/반복문/b_10950.java
+++ 백준/반복문/b_10950.java
@@ -1,19 +1,16 @@
-package b_problem;
+package b_3;
 
 import java.util.Scanner;
 
 public class b_10950 {
-	static int A;
-	static int B;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
 		int T = sc.nextInt();
-
-		for (int i = 0; i < T; i++) {
-			A = sc.nextInt();
-			B = sc.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
 			System.out.println(A + B);
 		}
 	}
