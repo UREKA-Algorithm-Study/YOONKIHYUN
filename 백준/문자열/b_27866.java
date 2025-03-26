09200e7fe71778fa056f78646ff7b9ecc7aa4695
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-26 17:15:46 +0900

    Create b_27866.java

---
+++ 백준/문자열/b_27866.java
@@ -0,0 +1,16 @@
+package b_5;
+
+import java.util.Scanner;
+
+public class b_27866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.next();
+		int i = sc.nextInt();
+		
+		System.out.println(S.charAt(i-1));
+	}
+
+}
