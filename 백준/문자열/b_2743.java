b3eb5b9460191c882160a2d6f8b377d8afc3892f
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-27 16:14:05 +0900

    Create b_2743.java

---
+++ 백준/문자열/b_2743.java
@@ -0,0 +1,14 @@
+package b_5;
+
+import java.util.Scanner;
+
+public class b_2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = sc.nextLine();	//한 줄 입력
+		System.out.println(word.length());
+	}
+
+}
