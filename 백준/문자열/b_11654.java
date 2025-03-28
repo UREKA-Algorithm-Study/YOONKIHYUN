e43e342e2f675e9644a02334e272e099510831aa
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-28 17:03:45 +0900

    Create b_11654.java

---
+++ 백준/문자열/b_11654.java
@@ -0,0 +1,16 @@
+package b_5;
+
+import java.util.Scanner;
+
+public class b_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char asc = sc.next().charAt(0);
+		
+		System.out.println((int) asc);
+		
+	}
+
+}
